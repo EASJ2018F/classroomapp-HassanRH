e88cb6daac0b9abdbb308c4370fd30e4e685289c
HassanRH <hassan---06@hotmail.com>
2018-01-30 18:56:52 +0100

    Opgave 1 og 2 færdig

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    //INSTANCEFIELDS
+    {
+        private string _klassenavn;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterstart;
+
+        //CONSTRUCTOR
+        public KlasseRum(String KlasseNavn, List<Studerende> KlasseList, DateTime SemesterStart)
+
+        {
+            KlasseNavn = _klassenavn;
+            KlasseList = _klasseliste;
+            SemesterStart = _semesterstart;
+        }
+        //PROPERTIES
+        public string Klassenavn
+        {
+            get { return _klassenavn; }
+            set { _klassenavn = value; }
+
+        }
+        public List<Studerende> klasseliste
+        {
+            get { return _klasseliste; }
+            set { _klasseliste = value; }
+        }
+
+        public DateTime semesterstart
+        {
+            get { return _semesterstart; }
+            set { _semesterstart = value; }
+        }
+
+    }
+}
+
+
+
+
+
+
---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        //INSTANCEFIELDS
+        string _navn;
+        int _fødselsmåned;
+        int _fødselsdag;
+
+        //CONSTRUCTOR
+        public Studerende(String Navn, int Fødselsmåned, int Fødselsdag)
+
+        {
+            Navn = _navn;
+            Fødselsmåned = _fødselsmåned;
+            Fødselsdag = _fødselsdag;
+        } 
+
+        //PROPERTIES
+        public string Navn { get; set; }
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { _fødselsmåned = value;}
+        }
+   
+        public int Fødselsdag
+        {
+            get { return _fødselsdag;}
+            set { _fødselsdag = value; }
+        }
+            
+    }
+}
